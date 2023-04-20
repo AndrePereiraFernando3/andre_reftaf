49fdb9dbf7d83cbb30862fcf81d0202e863e350f
andre <financeirobella3@gmail.com>
2023-04-20 14:30:49 -0300

    entrega

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 // Implemente aqui as funções
 
-
-
-
-
-function areCandidateResultsValid() {
-
+function abdominais(abs){
+    
+        return (abs >= 41)
+    
 }
+
+function swim (swimDistance, swimTime, diveTime){
+    
+        return ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30)
+    
+}
+
+function heigthGender ( gender, heigth){
+    if (gender == "male"){
+        return heigth >= 1.7
+    }
+    else{
+        return heigth >= 1.60
+    }
+}
+
+function barTest (gender,barReps,barSeconds){
+    if (gender == "male"){
+        return (barReps >= 6 || barSeconds >= 15)
+    } else {
+        return (barReps >= 5 || barSeconds >= 12)
+    } 
+}
+
+function runTest (gender, runDistance, runTime){
+    if (gender == "male"){
+        return (runDistance >= 3000 && runTime <= 720) || ( runDistance >= 5000 && runTime <= 1200)
+    } else{
+        return (runDistance >= 4000 && runTime <= 900) || ( runDistance >= 6000 && runTime <= 1320)
+    } 
+}
+
+
+
+function areCandidateResultsValid(gender, heigth, barReps, barSec, abs, runDistance, runTime, swimDistance, swimTime, diveTime ) {
+
+    
+    let absTestResult = abdominais(abs)
+    let swimTestResult = swim(swimDistance, swimTime, diveTime)
+    let heightGenderResult = heigthGender(gender, heigth)
+    let barTestResult = barTest(gender,barReps,barSec)
+    let runTestResult = runTest(gender, runDistance ,runTime)
+
+
+    if( absTestResult && swimTestResult && heightGenderResult && barTestResult && runTestResult){
+
+       return showMessage(true)
+
+        
+    }return showMessage(false)
+    
+}
+
 
 // Chamando a função com os parametros recebidos via script de teste
 // e atribuindo a variavel que devera ser mostrada no console
@@ -24,3 +75,8 @@ const areCandidateValid = areCandidateResultsValid(
 );
 
 // Chame aqui a função que mostra o resultado no console
+
+function showMessage(message){
+    console.log(message.toString().toUpperCase())
+}
+
